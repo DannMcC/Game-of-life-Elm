c4e75222ab8614bfedf510c10fb4bd85c3b2d140
Dan McCune <dmccune@hivelocity.net>
2017-07-26 10:07:41 -0400

    MUV

---
+++ GOL_grid.elm
@@ -0,0 +1,16 @@
+import Random
+
+import Html exposing (Html, Attribute, div, text)
+import Html.Attributes exposing (style)
+
+type alias Grid = List (List Bool)
+type Msg = Initialize Grid
+
+{-Model
+-}
+
+{-Update
+-}
+
+{-View
+-}
